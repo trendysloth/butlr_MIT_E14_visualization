5d1be2996cc6e25cfd687093e7189fe0528e4680
mlittleairport <kallywywu@gmail.com>
2019-05-20 00:51:17 -0700

    code clean up

--- js/script_e14.js
+++ js/script_e14.js
@@ -1,112 +1,59 @@
 var data = []
 var flatten_data = []
-var e14_participation_data = []
 var flatten_e14_data = []
+let r_values = []
+let y_values = []
+let company_names = []
 
-var accum_e14 = []
 
-var formatComma = d3.format(","),
-    formatDecimal = d3.format(".1f"), 
-    formatDecimalComma = d3.format(",.2f"),
-    formatSuffix = d3.format("s"),
-    formatSuffixDecimal1 = d3.format(".1s"),
-    formatSuffixDecimal2 = d3.format(".2s"),
-    formatMoney = function(d) { return "$" + formatDecimalComma(d); },
+var formatSuffixDecimal1 = d3.format(".1s"),
+    formatSuffixDecimal2 = d3.format(".2s")
     formatPercent = d3.format(",.2%");
 
-
 d3.csv("js/e14participation.csv", function(datapoints) {
-    // console.log(datapoints)
     datapoints.forEach(function(d) {
         var current = {
             name: d.Valuation,
             values: [
-                { date: "Angel", price: d.Angel },
-                { date: "SEED", price: d["SEED"] },
-                { date: "PRE A", price: d["PRE A"] },
-                { date: "B", price: d.B },
-                { date: "C", price: d.C },
+                { stage: "Angel", evaluation: d.Angel },
+                { stage: "SEED", evaluation: d["SEED"] },
+                { stage: "PRE A", evaluation: d["PRE A"] },
+                { stage: "B", evaluation: d.B },
+                { stage: "C", evaluation: d.C },
             ]
         }
-        var e14_current = {
-            name: d.Valuation,
-            values: [
-                { date: "Angel", price: d["Angel(e14)"] },
-                { date: "SEED", price: d["SEED(e14)"] },
-                { date: "PRE A", price: d["PRE A(e14)"] },
-                { date: "B", price: d["B(e14)"] },
-                { date: "C", price: d["C(e14)"] },
-            ]
-        }
-
-        accum_e14.push(
-            { name: d.Valuation, date: "Angel", price: parseInt(d["Angel(e14)"]) },
-            { name: d.Valuation, date: "SEED", price: parseInt(d["Angel(e14)"]) + parseInt(d["SEED(e14)"]) },
-            { name: d.Valuation, date: "PRE A", price: parseInt(d["Angel(e14)"]) + parseInt(d["SEED(e14)"]) + parseInt(d["PRE A(e14)"]) },
-            { name: d.Valuation, date: "B", price: parseInt(d["Angel(e14)"]) + parseInt(d["SEED(e14)"]) + parseInt(d["PRE A(e14)"]) + parseInt(d["B(e14)"]) },
-            { name: d.Valuation, date: "C", price: parseInt(d["Angel(e14)"]) + parseInt(d["SEED(e14)"]) + parseInt(d["PRE A(e14)"]) +parseInt( d["B(e14)"]) + parseInt(d["C(e14)"]) },
-        )
-
         flatten_data.push(
-            { name: d.Valuation, date: "Angel", price: d.Angel },
-            { name: d.Valuation, date: "SEED", price: d["SEED"] },
-            { name: d.Valuation, date: "PRE A", price: d["PRE A"] },
-            { name: d.Valuation, date: "B", price: d.B },
-            { name: d.Valuation, date: "C", price: d.C },
+            { name: d.Valuation, stage: "Angel", evaluation: d.Angel },
+            { name: d.Valuation, stage: "SEED", evaluation: d["SEED"] },
+            { name: d.Valuation, stage: "PRE A", evaluation: d["PRE A"] },
+            { name: d.Valuation, stage: "B", evaluation: d.B },
+            { name: d.Valuation, stage: "C", evaluation: d.C },
         )
         flatten_e14_data.push(
-            { name: d.Valuation, date: "Angel", price: d["Angel(e14)"] },
-            { name: d.Valuation, date: "SEED", price: d["SEED(e14)"] },
-            { name: d.Valuation, date: "PRE A", price: d["PRE A(e14)"] },
-            { name: d.Valuation, date: "B", price: d["B(e14)"] },
-            { name: d.Valuation, date: "C", price: d["C(e14)"] }
+            { name: d.Valuation, stage: "Angel", evaluation: d["Angel(e14)"] },
+            { name: d.Valuation, stage: "SEED", evaluation: d["SEED(e14)"] },
+            { name: d.Valuation, stage: "PRE A", evaluation: d["PRE A(e14)"] },
+            { name: d.Valuation, stage: "B", evaluation: d["B(e14)"] },
+            { name: d.Valuation, stage: "C", evaluation: d["C(e14)"] }
         )
         data.push(current);
-        e14_participation_data.push(e14_current);
-    })
 
-    let r_values = []
-    let y_values = []
-    let company_names = []
-    data.forEach(function(d) {
-        d.values.forEach(function(datapoint) {
-            if (parseInt(datapoint.price)) {
-                y_values.push(parseInt(datapoint.price))
-            } 
-        })
-        company_names.push(d.name)
-    })
-
-    flatten_e14_data.forEach(function(d) {
-        if (parseInt(d.price)){
-            r_values.push(parseInt(d.price))
-        } else {
-            r_values.push(0.000001)
+        if (!company_names.includes(d.Valuation)) {
+            company_names.push(d.Valuation)
         }
     })
-    console.log(e14_participation_data)
 
-    let company_list = []
-    let total_sum = 0
-    let sub_sums = []
-    let pie_chart_data = []
-
-    let accum_sum = []
-    data.forEach(function(d) {
-        sub_sum = 0
-        d.values.forEach(function(value) {
-            if (parseInt(value.price)) {
-                sub_sum = sub_sum + parseInt(value.price)
-            } 
-        })
-        sub_sums.push(sub_sum)
-        company_list.push(d.name)
-        total_sum = total_sum + sub_sum
+    flatten_data.forEach(function(d) {
+        if (parseInt(d.evaluation)) {
+            y_values.push(parseInt(d.evaluation))
+        }
     })
-    sub_sums.forEach(function(d, i) {
-        let company_valuation = {age: company_list[i], population:sub_sums[i]}
-        let total_valuation = {age: "total", population: total_sum}
-        pie_chart_data.push([company_valuation, total_valuation])
+    flatten_e14_data.forEach(function(d) {
+        if (parseInt(d.evaluation)){
+            r_values.push(parseInt(d.evaluation))
+        } else {
+            r_values.push(0)
+        }
     })
 
     var margin = {top: 50, right: 50, bottom: 50, left: 100}
@@ -118,23 +65,16 @@ d3.csv("js/e14participation.csv", function(datapoints) {
     var otherLinesOpacityHover = "1.0";
     var lineStroke = "4px";
     var lineStrokeHover = "6px";
-
     var circleOpacity = '1.0';
     var circleOpacityOnLineHover = "1.0"
-    var circleRadius = 3;
-    var circleRadiusHover = 25;
 
-
-    console.log(d3.extent(r_values))
     /* Scale */
-    var xScale = d3.scaleBand().domain(['Angel', 'SEED', 'PRE A', 'A', 'B', 'C']).range([0, width]); // output
+    var xScale = d3.scaleBand().domain(['Angel', 'SEED', 'PRE A', 'A', 'B', 'C']).range([0, width]); 
     var yScale = d3.scaleLog().domain(d3.extent(y_values)).range([height, 0]);
     var rScale = d3.scaleLinear().domain(d3.extent(r_values)).range([10, 25]);
-    var color = d3.scaleOrdinal(d3.schemeCategory10)
-    var color2 = d3.scaleOrdinal(d3.schemeCategory10).domain(['A', 'B', 'C', 'D', 'E'])
-    // console.log(color2('A'))
+    var color = d3.scaleOrdinal(d3.schemeCategory10).domain(company_names)
 
-    // /* Add SVG */
+    /* Add SVG */
     var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -142,19 +82,14 @@ d3.csv("js/e14participation.csv", function(datapoints) {
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     /* Add line into SVG */
-    var line = d3.line()
-    .x(function(d) {
-        return xScale(d.date)
-    })
-    .y(function(d) {
-        if (d.price !== '0' && d.price !== 'NOT THERE YET') {
-            return yScale(d.price)
-        } 
-    })
+    var line = d3.line().x(function(d) { return xScale(d.stage) })
+                        .y(function(d) {
+                            if (d.evaluation !== '0' && d.evaluation !== 'NOT THERE YET') {
+                                return yScale(d.evaluation)
+                            } 
+                        })
 
-    let lines = svg.append('g')
-    .attr('class', 'lines')
-
+    let lines = svg.append('g').attr('class', 'lines')
     lines.selectAll('.line-group')
     .data(data).enter()
     .append('g')
@@ -162,23 +97,21 @@ d3.csv("js/e14participation.csv", function(datapoints) {
     .on("mouseover", function(d, i) {
         svg.append("text")
             .attr("class", "title-text")
-            .style("fill", color(i))        
-            .text('[company_' + d.name + ']')
+            .style("fill", color(d.name))        
+            .text('company_' + d.name)
             .attr("text-anchor", "middle")
             .attr("x", (width - margin.left - margin.right)/2)
             .attr("y", 20)
             .attr("text-anchor", "middle")
         })
-    .on("mouseout", function(d) {
-        svg.select(".title-text").remove();
-    })
+    .on("mouseout", () => svg.select(".title-text").remove())
     .append('path')
     .attr('class', 'line')  
-    .attr('d', function(d, i) {
-        return line(d.values.filter(item => (item.price !== 'NOT THERE YET') && 
-                                   (item.price !== "0") && (item.price !== '')))
+    .attr('d', function(d) {
+        return line(d.values.filter(item => (item.evaluation !== 'NOT THERE YET') && 
+                                   (item.evaluation !== "0") && (item.evaluation !== '')))
     })
-    .style('stroke', (d, i) => color(i))
+    .style('stroke', (d) => color(d.name))
     .style("stroke-width", lineStroke)
     .style('opacity', lineOpacity)
     .on("mouseover", function(d) {
@@ -202,6 +135,7 @@ d3.csv("js/e14participation.csv", function(datapoints) {
         })
     .attr("transform", "translate(" + xScale.bandwidth() / 2 + ",0)");
 
+    /* Add Circles into SVG */
     svg.selectAll("circle")
     .data(flatten_data)
     .enter()
@@ -209,75 +143,47 @@ d3.csv("js/e14participation.csv", function(datapoints) {
     .attr("class", "circle")  
     .append("circle")
     .attr("cx", function(d, i) {
-        // console.log(d)
-        return xScale(d.date)
+        return xScale(d.stage)
     })
     .attr("cy", function(d, i) {
-        if (d.price !== '' && d.price !== '0' && d.price !== 'NOT THERE YET') {
-            return yScale(d.price)
+        if (d.evaluation !== '' && d.evaluation !== '0' && d.evaluation !== 'NOT THERE YET') {
+            return yScale(d.evaluation)
         }
     })
     .attr("r", function(d, i) {
-        // console.log(d, flatten_e14_data[i])
-        if (d.price !== '' && d.price !== '0' && d.price !== 'NOT THERE YET') {
-            if (flatten_e14_data[i].price !== '0' && flatten_e14_data[i].price !== 'NOT THERE YET') {
-                return rScale(parseInt(flatten_e14_data[i].price))
+        if (d.evaluation !== '' && d.evaluation !== '0' && d.evaluation !== 'NOT THERE YET') {
+            if (flatten_e14_data[i].evaluation !== 'NOT THERE YET') {
+                return rScale(parseInt(flatten_e14_data[i].evaluation))
             } 
-            if (flatten_e14_data[i].price === '0') {
-                return rScale(parseInt(0.000001))
-            }
         }
     })
     .attr("transform", "translate(" + xScale.bandwidth() / 2 + ",0)")
     .style("fill", function(d, i) {
-        return (color2(d.name))
+        return (color(d.name))
     })
-    // .style('opacity', circleOpacity)
-    // .on("mouseover", function(d) {
-    //     d3.select(this)
-    //     .transition()
-    //     .duration(duration)
-    //     .attr("r", circleRadiusHover);
-    // })
-    // .on("mouseout", function(d) {
-    //     d3.select(this) 
-    //     .transition()
-    //     .duration(duration)
-    //     .attr("r", function(d, i) {
-    //         // yScale(d.price)
-    //         if (d.price !== '' && d.price !== '0' && d.price !== 'NOT THERE YET') {
-    //             if (flatten_e14_data[i].price !== '0' && flatten_e14_data[i].price !== 'NOT THERE YET') {
-    //                 return rScale(parseInt(flatten_e14_data[i].price))
-    //             } else {
-    //                 return rScale(parseInt(0.000001))
-    //             }
-                
-    //         }
-    //     });  
-    // })
-        
+    
+    /* Add text next to circles */
     svg.selectAll("text")
     .data(flatten_data)
     .enter()
     .append("text")
     .text(function(d, i) {
-        // console.log(flatten_e14_data)
-        if (d.price !== '' &&  d.price !== '0' && d.price !== 'NOT THERE YET') {
-            let tmp = parseInt(flatten_e14_data[i].price) / parseInt(d.price)
-            if (parseInt(d.price) >= 1000000000) {
-                return (d.price / 1000000000) + '000M' + " [" + (tmp * 100).toFixed(2).toString() + "%]"
+        if (d.evaluation !== '' &&  d.evaluation !== '0' && d.evaluation !== 'NOT THERE YET') {
+            let tmp = parseInt(flatten_e14_data[i].evaluation) / parseInt(d.evaluation)
+            if (parseInt(d.evaluation) >= 1000000000) {
+                return (d.evaluation / 1000000000) + '000M' + " [" + formatPercent(tmp) + "]"
             }
-            return formatSuffixDecimal2(d.price).toString() + " [" + (tmp * 100).toFixed(2).toString() + "%]"
+            return formatSuffixDecimal2(d.evaluation) + " [" + formatPercent(tmp) + "]"
         }
     })
     .attr("x", function(d) {
-        if (d.price !== '' && d.price !== '0' && d.price !== 'NOT THERE YET') {
-            return xScale(d.date)
+        if (d.evaluation !== '' && d.evaluation !== '0' && d.evaluation !== 'NOT THERE YET') {
+            return xScale(d.stage)
         }
     })
     .attr("y", function(d) {
-        if (d.price !== '' && d.price !== '0' && d.price !== 'NOT THERE YET') {
-            return yScale(parseInt(d.price))
+        if (d.evaluation !== '' && d.evaluation !== '0' && d.evaluation !== 'NOT THERE YET') {
+            return yScale(parseInt(d.evaluation))
         }
     })
     .attr("font_family", "sans-serif")  // Font type
@@ -287,31 +193,31 @@ d3.csv("js/e14participation.csv", function(datapoints) {
     })
     .attr("transform", "translate(" + xScale.bandwidth() * 0.62 + "," + "0)");
 
-
-    var e14 = svg.selectAll('#e14')
+    /* Add text inside circles */
+    svg.selectAll('#e14')
     .data(flatten_data)
     .enter() 
     .append("text")
     .text(function(d, i) {
-        if (d.price !== '' &&  d.price !== '0' && d.price !== 'NOT THERE YET') {
-            if (flatten_e14_data[i].price !== '0') {
-                return formatSuffixDecimal1(flatten_e14_data[i].price)
+        if (d.evaluation !== '' &&  d.evaluation !== '0' && d.evaluation !== 'NOT THERE YET') {
+            if (flatten_e14_data[i].evaluation !== '0') {
+                return formatSuffixDecimal1(flatten_e14_data[i].evaluation)
             }
         }
     })
     .attr("x", function(d) {
-        if (d.price !== '' && d.price !== '0' && d.price !== 'NOT THERE YET') {
-            return xScale(d.date)
+        if (d.evaluation !== '' && d.evaluation !== '0' && d.evaluation !== 'NOT THERE YET') {
+            return xScale(d.stage)
         }
     })
     .attr("y", function(d) {
-        if (d.price !== '' && d.price !== '0' && d.price !== 'NOT THERE YET') {
-            return yScale(parseInt(d.price))
+        if (d.evaluation !== '' && d.evaluation !== '0' && d.evaluation !== 'NOT THERE YET') {
+            return yScale(parseInt(d.evaluation))
         }
     })
     .attr("dy", "2px")
-    .attr("font_family", "sans-serif")  // Font type
-    .attr("font-size", "9px")  // Font size
+    .attr("font_family", "sans-serif")
+    .attr("font-size", "9px")
     .attr("fill", function(d) { 
         return 'white'
     })
@@ -320,7 +226,6 @@ d3.csv("js/e14participation.csv", function(datapoints) {
         return "translate(" + xScale.bandwidth() * 0.5 + "," + "0)"
     })
     
-
     /* Add Axis into SVG */
     var xAxis = d3.axisBottom(xScale).ticks(5);
     var yAxis = d3.axisLeft(yScale).ticks(5);
